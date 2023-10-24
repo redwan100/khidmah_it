83458664b9ed8ffe3960bb5d22aacc31194f8759
Redwan islam <redwanislam2760@gmail.com>
2023-10-24 12:23:53 +0600

    complete services page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/HeroSection";
 import AboutUs from "./aboutUs/AboutUs";
+import OurServices from "./services/OurServices";
 
 const Home = () => {
   return (
     <div className="">
       <HeroSection />
       <AboutUs />
+      <OurServices />
     </div>
   );
 };
--- src/pages/shared/Button.jsx
+++ src/pages/shared/Button.jsx
@@ -2,7 +2,7 @@ import shape from "../../assets/Shapes.svg";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 const Button = ({ text = "btn text" }) => {
   return (
-    <button className="bg-btnBg py-2 px-4 rounded-sn relative flex items-center gap-2 drop-shadow-sm">
+    <button className="bg-btnBg py-2 px-4 rounded-sn relative flex items-center gap-2 drop-shadow-sm text-zinc-50 capitalize">
       <img
         src={shape}
         alt=""
